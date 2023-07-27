b218032c366785c5247d717b522f8367ab4d14b7
HWISU96 <soshyzx123@naver.com>
2023-07-27 17:45:50 +0900

    2557번

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    // 자바 class Main 선언, 백준에서는 클래스명을 Main으로 선언해야함
+// 자바 class Main 선언, 백준에서는 클래스명을 Main으로 선언해야함
     public static void main(String[] args)
-// 자바 main 함수를 리턴값이 없이 메모리에 항상 상주하게 선언
+    // 자바 main 함수를 리턴값이 없이 메모리에 항상 상주하게 선언
     {
         System.out.println("Hello World!");
-// 문자열을 출력시키는 System.out.println 메소드로 문자열 Hello World! 출력
+        // 문자열을 출력시키는 System.out.println 메소드로 문자열 Hello World! 출력
     }
 }
